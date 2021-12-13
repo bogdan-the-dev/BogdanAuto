c8bfb9acbdd1b5026c166e89bd9a113c80c790dc
Bogdan <bogdantamba55@gmail.com>
2021-12-13 23:06:25 +0200

    Add project files.

---
+++ Bogdan Auto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bogdan_Auto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
