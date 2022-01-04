4e9d04df14ed2c0f8c36b3140e1130f8487fe022
Bogdan <bogdantamba55@gmail.com>
2022-01-04 18:41:47 +0200

    start working on index page and finished privacy page

--- Bogdan Auto/Controllers/HomeController.cs
+++ Bogdan Auto/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Bogdan_Auto.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
