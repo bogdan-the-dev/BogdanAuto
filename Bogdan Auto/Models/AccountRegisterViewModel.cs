8d80587c03a4665341feedae1523094c23d4419d
Bogdan <bogdantamba55@gmail.com>
2022-01-12 02:24:02 +0200

    Account Register Model

---
+++ Bogdan Auto/Models/AccountRegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bogdan_Auto.Models
+{
+    public class AccountRegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Account type")]
+        [DataType(DataType.Text)]
+        public string AccountType { get; set; }
+    }
+}
